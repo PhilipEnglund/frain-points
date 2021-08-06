d27a6e7580183d9fa43ad370661de81474b05f5d
Philip Englund <frainbreese@gmail.com>
2021-08-06 15:45:41 +0200

    run build

--- dist/Fp.module.d.ts
+++ dist/Fp.module.d.ts
@@ -1,7 +1,16 @@
 export declare namespace Fp {
+    /**
+     * @type Point
+     * @description Array Tuple representing x and y coordinates
+     */
     type Point = [number, number];
-    function multiply({ originalPoints, radius, }: {
-        originalPoints: Array<Point>;
-        radius?: number;
-    }): Array<Point>;
+    /**
+     * @function smoothen
+     * @description Takes original points and returns a new array of points, applying
+     * an implementation of Chaikins algorithm to smoothen out curves.
+     *
+     * @param {Fp.Point[]} originalPoints - an array of points
+     * @param {number} radius - non required param of radius, must be between 0.1-0.5
+     */
+    function smoothen(originalPoints: Array<Point>, radius?: number): Array<Point>;
 }
--- dist/Fp.module.js
+++ dist/Fp.module.js
@@ -13,16 +13,23 @@ var Fp;
         var p2x = _b[0], p2y = _b[1];
         return [(1 - t) * p1x + t * p2x, (1 - t) * p1y + t * p2y];
     }
-    function multiply(_a) {
-        var originalPoints = _a.originalPoints, _b = _a.radius, radius = _b === void 0 ? 0.5 : _b;
+    /**
+     * @function smoothen
+     * @description Takes original points and returns a new array of points, applying
+     * an implementation of Chaikins algorithm to smoothen out curves.
+     *
+     * @param {Fp.Point[]} originalPoints - an array of points
+     * @param {number} radius - non required param of radius, must be between 0.1-0.5
+     */
+    function smoothen(originalPoints, radius) {
         if (radius > 0.5 || radius < 0.1) {
             throw new Error("Param 'options' can't be less than 0.1 or higher than 0.5");
         }
-        return originalPoints.reduce(function (acc, point, i, points) { return __spreadArray(__spreadArray(__spreadArray([], acc), [
+        return __spreadArray([], originalPoints).reduce(function (acc, point, i, points) { return __spreadArray(__spreadArray(__spreadArray([], acc), [
             lerp(point, i === 0 ? points[points.length - 1] : points[i - 1], radius),
             point
         ]), lerp(point, i === points.length - 1 ? points[0] : points[i + 1], radius)); }, []);
     }
-    Fp.multiply = multiply;
+    Fp.smoothen = smoothen;
 })(Fp = exports.Fp || (exports.Fp = {}));
 //# sourceMappingURL=Fp.module.js.map
